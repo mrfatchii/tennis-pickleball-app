c21af4e2512edd0f9210da4391be52d87454eca1
mrfatchii <mrfatchii@users.noreply.github.com>
2026-09-18 15:25:24 +0000

    feat: 生成项目Code Wiki文档
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,24 @@
-{
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "plugins": ["react", "react-hooks"],
-  "parserOptions": {
-    "ecmaVersion": 2022,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  },
-  "env": {
-    "browser": true,
-    "es2022": true,
-    "node": true
-  },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  },
-  "rules": {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/prop-types": "off",
-    "no-unused-vars": "warn"
+import reactHooks from 'eslint-plugin-react-hooks'
+
+export default [
+  {
+    plugins: { 'react-hooks': reactHooks },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      },
+      globals: {
+        console: 'readonly',
+        window: 'readonly',
+        document: 'readonly'
+      }
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    },
+    ignores: ['dist', 'node_modules', '.git', '.github']
   }
-}
+]
